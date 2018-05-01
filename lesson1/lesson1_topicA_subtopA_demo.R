407872893a0efa8425914661a5a93dd270302841
klmedeiros <kaelenmedeiros@gmail.com>
2018-04-30 19:07:01 -0500

    adding code to lesson 1

---
+++ lesson1/lesson1_topicA_subtopA_demo.R
@@ -0,0 +1,19 @@
+#custom function
+add <- function(x, y){
+  x + y
+}
+
+#how it's used
+add(3, 4)
+
+add(100, 12)
+
+add(838383838833838301920, 1)
+
+#check working directory
+getwd()
+
+#change working directory
+setwd("/a location/on the computer/you're using")
+
+save.image("lesson1_demo.RData")
