6d055e80c5a93f4b28ddd6a6928916f9136a83be
klmedeiros <kaelenmedeiros@gmail.com>
2018-05-28 21:21:11 -0400

    lesson 3 edits

--- lesson3/lesson3_topicB_subtopA_demo.R
+++ lesson3/lesson3_topicB_subtopA_demo.R
@@ -3,22 +3,3 @@ install.packages("rpivotTable", "htmlwidgets")
 
 library(rpivotTable)
 rpivotTable(diamonds)
-
-#demo starts here:
-
-table(iris$Species)
-
-table(diamonds$cut, diamonds$color)
-
-table(diamonds$cut, diamonds$color, diamonds$clarity)
-
-library(dplyr)
-
-diamonds %>% group_by(cut, color, clarity) %>% summarise(n())
-
-table(mtcars$mpg)
-
-#summarising with dplyr
-diamonds %>% group_by(cut) %>% summarise(mean = mean(price), median = median(price))
-
-diamonds %>% filter(color != "D" & color != "J") %>% group_by(cut) %>% summarise(mean = mean(price))
