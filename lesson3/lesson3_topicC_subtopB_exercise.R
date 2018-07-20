4beb1dd3f3b58150da8e24142a5ce7c5b164fa26
Kaelen Medeiros <kaelenmedeiros@gmail.com>
2018-07-19 22:53:48 -0400

    commit lesson3 changes

---
+++ lesson3/lesson3_topicC_subtopB_exercise.R
@@ -0,0 +1,42 @@
+#install and load readr
+install.packages("readr")
+library(readr)
+
+#load in students and students 2 from github
+students <- read_csv("https://raw.githubusercontent.com/TrainingByPackt/Beginning-Data-Science-with-R/master/lesson3/students.csv")
+students2 <- read_csv("https://raw.githubusercontent.com/TrainingByPackt/Beginning-Data-Science-with-R/master/lesson3/students2.csv")
+
+#use str to examine both datasets
+str(students)
+str(students2)
+
+#merge
+students_combined <- merge(students, students2, by = "ID")
+dim(students_combined)
+
+students_combined2 <- merge(students, students2, by = "ID", all.y = TRUE)
+dim(students_combined2)
+
+#join
+install.packages("dplyr")
+library(dplyr)
+
+#right join
+students_right_join <- right_join(students, students2, by = "ID")
+dim(students_right_join)
+
+#anti join
+students_anti_join <- anti_join(students, students2, by = "ID") 
+dim(students_anti_join)
+
+
+#no by variable
+students_merge_noby <- merge(students, students2)
+students_join_noby <- right_join(students, students2)
+
+#join variable named different things
+colnames(students)[6] <- "StudentID"
+
+#specify the different merge variables
+students_merge_diff <- merge(students, students2, by.x = "StudentID", by.y = "ID")
+students_join_diff <- right_join(students, students2, by = c("StudentID" = "ID"))
