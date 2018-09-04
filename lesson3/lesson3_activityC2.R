87a0b14c026d978364649f1869dd36460a889452
pjmathews <pjmathews@users.noreply.github.com>
2018-09-04 18:00:38 +0530

    Update lesson3_activityC2.R

--- lesson3/lesson3_activityC2.R
+++ lesson3/lesson3_activityC2.R
@@ -1,3 +1,4 @@
+#tested and working
 #read in students data
 library(readr)
 students <- read_csv("https://github.com/TrainingByPackt/Beginning-Data-Science-with-R/blob/master/lesson1/students.csv")
